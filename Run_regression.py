7771934c53143bbf818e057296cef09a34a26197
Your Name <you@example.com>
2020-02-25 14:18:39 -0500

    add regression utils

--- Run_regression.py
+++ Run_regression.py
@@ -4,23 +4,47 @@ import os
 import pandas as pd
 from regression_utils import MakeRegression,GetModel
 from sklearn.model_selection import StratifiedKFold
-####Load PSD and csv files ####
+
+######################## Load PSD and csv files ################################
+
 data_path=os.getcwd()
 Psd_file=os.path.join(data_path,'PSD_all_bands.mat')
 PSD_all_bands=sio.loadmat(Psd_file)['psd']
 csv_file=os.path.join(data_path,'data_N45_all.csv')
 df=pd.read_csv(csv_file,sep=';')
-target_name='Treatment'
+
+########################### Test On/off  #######################################
+
+test=True
+if test:
+    bandes=['Delta']
+else:
+    bandes=['Delta','Theta','Alpha','Beta','Gamma1','Gamma2','Gamma3','GammaL']
+
+####################### Set regression parameters ##############################
+
+target_name='Radio'# Set the target name
 target=df[target_name].tolist()
 
+regressor='MLP'  # Regressor name
+inner_cv=5         # Inner cross validation used to optimise the model's params
+outer_cv=10        # Outer cv used to train and test data
+optimise=True           # Turn to True if you want to optimise the model
+FeatSelect=False   # Set to True if you want to run feature selection
+target_name='Radio'
+target=df[target_name].tolist()
 
-bandes=['Delta','Theta','Alpha','Beta','Gamma1','Gamma2','Gamma3','GammaL']
-regressor='Lasso'
-inner_cv=None
-outer_cv=10
-feature_selection=False
+########################### Run Regression #####################################
 for bdi,bd in enumerate(bandes):
+    print('Runing regression for bande {}'.format(bd))
     X=np.squeeze(PSD_all_bands[:,bdi,:]).T
-    model=GetModel(regressor)
-    regsc=MakeRegression(model=model,X=X,y=target,inner_cv=None,outer_cv=None,feat_selection=False)
-    print(regsc)
+    model=GetModel(regname=regressor,
+                   optimisation=optimise,
+                   cv=inner_cv)
+    results=MakeRegression(model=model,
+                        X=X,
+                        y=target,
+                        inner_cv=inner_cv,
+                        outer_cv=outer_cv,
+                        feat_selection=FeatSelect)
+    print(results)
--- regression_utils.py
+++ regression_utils.py
@@ -2,25 +2,70 @@ import numpy as np
 import os
 import scipy.io as sio
 from sklearn.linear_model import LinearRegression as Linreg
+from sklearn.neural_network import MLPClassifier
 from sklearn.linear_model import Lasso
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import cross_validate
 from sklearn.svm import SVR
+from sklearn.model_selection import RandomizedSearchCV
 
-def GetModel(regname=None):
+def GetModel(regname=None,optimisation=False,cv=None):
     if regname.lower()=='lasso':
-        regressor = Lasso()
+        lasreg=Lasso()
+        if optimisation:
+            p_grid = {}
+            p_grid['alpha']=np.logspace(-10, 10, 50)
+
+            n_iter_search=50
+            Rnd_Srch = RandomizedSearchCV(lasreg,
+                                          param_distributions=p_grid,
+                                          n_iter=n_iter_search,
+                                          cv=cv,iid=True)
+            regressor = make_pipeline(StandardScaler(),Rnd_Srch)
+        else:
+            regressor = lasreg
     if regname.lower()=='linear_model':
         regressor=Linreg()
     if regname.lower()=='svr':
-        regressor= SVR()
+        svr= SVR()
+        if optimisation==False:
+            regressor=svr
+        else:
+            p_grid = {}
+
+            p_grid['gamma']= ['scale','auto']
+            p_grid['C']= [1, 5,10,15,20,25]
+            n_iter_search=10
+            Rnd_Srch = RandomizedSearchCV(svr, param_distributions=p_grid,
+                                           n_iter=n_iter_search, cv=cv,iid=True)
+            regressor = make_pipeline(StandardScaler(),Rnd_Srch)
+    if regname.lower()=='mlp':
+        reg=MLPClassifier()
+        if optimisation:
+            p_grid = {}
+            p_grid['hidden_layer_sizes']=np.arange(10)+1
+            p_grid['activation']=['identity', 'logistic', 'tanh', 'relu']
+
+            n_iter_search=50
+            Rnd_Srch = RandomizedSearchCV(reg,
+                                          param_distributions=p_grid,
+                                          n_iter=n_iter_search,
+                                          cv=cv,iid=True)
+            regressor = make_pipeline(StandardScaler(),Rnd_Srch)
+        else:
+
+            regressor=reg
     return regressor
 
 
 def MakeRegression(model=[],X=[],y=[],inner_cv=None,outer_cv=None,feat_selection=False):
     if inner_cv==None:
 
+        scores = cross_validate(model, X, y, cv=outer_cv,
+                                scoring=('r2', 'neg_mean_squared_error'),
+                                return_train_score=True)
+    else:
         scores = cross_validate(model, X, y, cv=outer_cv,
                                 scoring=('r2', 'neg_mean_squared_error'),
                                 return_train_score=True)
